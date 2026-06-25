17d1ccea2fa86f3d4b9be92369efdd8976e4e9fd
aswinvijayan-vonnue <aswin.vijayan@vonnue.com>
2026-06-25 13:30:57 +0530

    Task 6 of day 2 - accordian using javascript

---
+++ Day2/accordian.js
@@ -0,0 +1,51 @@
+let buttons = document.querySelectorAll(".header-button");
+let key=sessionStorage.getItem("openat");
+console.log(key);
+if(key>=0){
+     const panel=buttons[key].nextElementSibling;
+     panel.classList.add("active");
+     buttons[key].parentElement.setAttribute("aria-expanded","true");
+
+ }
+buttons.forEach((button, index) => {
+  button.addEventListener("click", function (event) {
+    let panel = this.nextElementSibling;
+    console.log(index);
+    panel.classList.toggle("active");
+    const isopen = panel.classList.contains("active");
+    this.parentElement.setAttribute("aria-expanded", isopen);
+    if(isopen){
+        sessionStorage.setItem("openat",index);
+        console.log(`stored ${sessionStorage.getItem("openat")}`);
+    }
+    console.log(panel);
+    console.log(buttons.length);
+    for (let i = 0; i < buttons.length; i++) {
+      if (i != index) {
+        buttons[i].parentElement.setAttribute("aria-expanded", "false");
+        let body = buttons[i].nextElementSibling;
+        body.classList.remove("active");
+      }
+    }
+  });
+  button.addEventListener("keydown", (event) => {
+    console.log(event.key);
+    let nextind = 0;
+    if (event.key == "ArrowDown") {
+      event.preventDefault();
+      nextind = (index + 1) % buttons.length;
+      buttons[nextind].focus();
+    } else if (event.key == "ArrowUp") {
+      event.preventDefault();
+      if (index == 0) nextind = buttons.length - 1;
+      else nextind = index - 1;
+      buttons[nextind].focus();
+    } else if (event.key == "Home") {
+      event.preventDefault();
+      buttons[0].focus();
+    } else if (event.key == "End") {
+      event.preventDefault();
+      buttons[buttons.length - 1].focus();
+    }
+  });
+});
