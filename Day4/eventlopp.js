9cff4b95284cfa8ba12f3058ff79be7f683ec74f
aswinvijayan-vonnue <aswin.vijayan@vonnue.com>
2026-07-01 16:52:10 +0530

    feat: task 1 of day 4

--- Day4/eventlopp.js
+++ Day4/eventlopp.js
@@ -52,3 +52,97 @@ function test4(){
 }
 console.log("\n calling function 4");
 test4();
+function test5(){
+    setTimeout(()=>{
+        Promise.resolve()
+        .then(()=>{
+            console.log("1");
+        });
+        console.log("2");
+        queueMicrotask(()=>console.log("3"));
+    },0)
+    //setTimeout directly enqueue to task queue once when it reaches in call stack promise enqueue into multitask queue log 2 and enqueue queueMicrotask in microtask queue, thus order is 2-1-3
+}
+test5();
+function test6(){
+    setTimeout(()=>{
+        console.log("1");
+        Promise.resolve()
+        .then(()=>{
+            console.log("2");
+        })
+
+    },1000);
+    setTimeout(()=>{
+        console.log("3");
+        queueMicrotask(()=>console.log("4"));
+    },0);
+    //first settimeout will wait for 1 second in webapi 2nd setTimeout directly enqueue to task queue then first, so order is 3-4-1-2
+}
+test6();
+function test7(){
+    Promise.resolve()
+    .then(()=>{
+        Promise.resolve()
+        .then(()=>{
+            Promise.resolve()
+            .then(()=>{
+                setTimeout(() => {
+                    console.log("4");
+                }, 0);
+                console.log("3");
+                Promise.resolve()
+                .then(()=>console.log("5"));
+            })
+            console.log("2");
+        })
+        console.log("1");
+    })
+    //outermost promise will run first. no code in call stack, then nested two promises, in the inner most promise there are three code snipptes console.log first then the promise invoke, atlast the set timeout function. order 1-2-3-5-4 
+}
+test7();
+function test8(){
+    setTimeout(()=>{
+        setTimeout(()=>{
+            setTimeout(()=>{
+                queueMicrotask(()=>console.log("4"));
+                console.log("2");
+            },1000);
+        },1000)
+        setTimeout(()=>{
+            console.log("3");
+        },3000);
+        console.log("1");
+    },0)
+    //outermost setTimeout execute first, after-inner there are two setTimeout functions one with delay 1 second another with delay 3 second so 1 second delayed timeout function execute next logs 2 then 4 atlast log 3 order 1-2-4-3
+}
+test8();
+function test9(){
+    queueMicrotask(()=>{
+        queueMicrotask(()=>{
+            setTimeout(()=>{
+                console.log("3");
+            },0);
+            console.log("2");
+        })
+        console.log("1");
+    });
+    //outermost queueMicrotask execute first then nested one,atlast setTimeout function will execute. Thus order is 1-2-3
+}
+test9();
+function test10(){
+    queueMicrotask(()=>{
+        setTimeout(()=>{
+            console.log("4")
+
+        },1000);
+        Promise.resolve()
+        .then(()=>{
+            console.log("3");
+        })
+        console.log("2")
+    })
+    console.log("1");
+}
+//firstly in call stack there console.log,after that queueMicrotask will fed from microtask queue-setTimeout function fed to webapi once delay reached then enqued to task queue Promise is directly enqued to microtask queue and logs 2,then Promise is invoed last timeout function. order 1-2-3-4
+test10();
