005549931c48bc6f3968e7ac8b8cc860ccfe1fae
aswinvijayan-vonnue <aswin.vijayan@vonnue.com>
2026-07-16 17:13:20 +0530

    feat:week 4 task 8 of day 2

--- w4-day1/counter.js
+++ w4-day1/counter.js
@@ -1,6 +1,8 @@
 const cards=document.querySelectorAll('.card');
 let startTime;
 
+const easeout=(t)=>t * (2-t);
+
 const counterObserver=new IntersectionObserver((entries,observer)=>{
     entries.forEach((entry)=>{
         if(entry.isIntersecting){
@@ -13,7 +15,8 @@ const counterObserver=new IntersectionObserver((entries,observer)=>{
             const step=(current)=>{
                 if(!start) start=current;
                 const progress=Math.min((current-start)/duration,1);
-                timer.textContent=Math.floor(progress*target);
+                const easeProgress=easeout(progress);
+                timer.textContent=Math.floor(easeProgress*target);
                 if(progress<1){
                     requestAnimationFrame(step);
                 }
@@ -27,3 +30,28 @@ const counterObserver=new IntersectionObserver((entries,observer)=>{
     })
 },{threshold:0.25})
 cards.forEach((card)=>counterObserver.observe(card));
+
+function startUpload(){
+    const progressBar=document.querySelector('.progress-bar');
+    const duration=10000;
+    let start=null;
+    
+   function step(current){
+    if(!start) start=current;
+    const elapsed=current-start;
+    const progress=Math.min(elapsed/duration,1);
+    const percentage=progress*100;
+    progressBar.style.width=`${percentage}%`;
+    if(progress<100){
+         setTimeout(()=>{
+        requestAnimationFrame(step);
+    },100)
+    }
+
+   }
+   setTimeout(()=>{
+        requestAnimationFrame(step);
+    },1000)
+   
+}
+ document.querySelector('.upload-button').addEventListener('click',startUpload);
